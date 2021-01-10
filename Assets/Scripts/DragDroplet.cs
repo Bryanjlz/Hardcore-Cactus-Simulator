eb33e79a293c8910ca18a5cc4a4c274e8298c9eb
Kervina <45807615+Kervina@users.noreply.github.com>
2021-01-10 14:38:32 -0500

    water

---
+++ Assets/Scripts/DragDroplet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDroplet : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    public bool isBeingHeld = true;
+    Vector3 mousePos;
+
+    private void Update()
+    {
+        Debug.Log(isBeingHeld);
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            isBeingHeld = false;
+        }
+
+        if (isBeingHeld == true)
+        {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            startPosX = mousePos.x;
+            startPosY = mousePos.y;
+
+            this.gameObject.transform.localPosition = new Vector3(startPosX, startPosY, 0);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/WateringCan.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCan : MonoBehaviour
+{
+    Vector3 mousePos;
+    public GameObject waterDroplet;
+    private GameObject thisWater;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisWater = Instantiate(waterDroplet);
+        thisWater.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            thisWater.GetComponent<DragDroplet>().isBeingHeld = true;
+            thisWater.transform.localPosition = new Vector3(mousePos.x, mousePos.y, 0);
+            thisWater.SetActive(true);
+        }
+    }
+}
